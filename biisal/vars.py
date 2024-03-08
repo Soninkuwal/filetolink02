e8621334558ca1b8b7e2b0923064c9cae3acefa6
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-03-08 20:53:03 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -36,9 +36,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',True))
     if HAS_SSL:
-        URL = "https://filetolink-kanhaiyamovie.koyeb.app/".format(FQDN)
+        URL = "http://{}/".format(FQDN)
     else:
-        URL = "https://filetolink-kanhaiyamovie.koyeb.app/".format(FQDN)
+        URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', ''))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'bisal_files')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
