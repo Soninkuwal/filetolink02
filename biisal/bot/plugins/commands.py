4a0e3d30eeffe39109caf998fd847976b8523fbe
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-05-03 19:18:19 +0530

    📌 commands.py 📌

--- biisal/bot/plugins/commands.py
+++ biisal/bot/plugins/commands.py
@@ -13,7 +13,7 @@ from pyrogram.errors import UserNotParticipant
 from biisal.utils.file_properties import get_name, get_hash, get_media_file_size
 db = Database(Var.DATABASE_URL, Var.name)
 from pyrogram.types import ReplyKeyboardMarkup
-from biisal.vars import bot_name , bisal_channel , bisal_grp
+from kanha.vars import bot_name , kanha_channel , kanha_grp
 
 
 SRT_TXT = """<b>ᴊᴀɪ sʜʀᴇᴇ ᴋʀsɴᴀ {}!,
