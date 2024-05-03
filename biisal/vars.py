4a77461996885433d3bb13ec6fb82b5564a8f769
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-05-03 13:23:54 +0530

    📌 vars.py ✅

--- biisal/vars.py
+++ biisal/vars.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 bot_name = "KANHA FILES TO LINK 🖇️ BOT"
 bisal_channel = "https://t.me/Sonickuwalupdate"
-bisal_grp = "https://telegram.me/Chatkanhabot"
+bisal_grp = "https://t.me/SONICKUWALMOVIE"
 
 class Var(object):
     MULTI_CLIENT = False
