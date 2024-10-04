76e9f6e5188d7385cb9e85dfa0d673e914e6dffd
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-10-04 11:07:23 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -12,18 +12,18 @@ kanha_grp = "https://t.me/SONICKUWALMOVIE"
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '20945078'))
-    API_HASH = str(getenv('API_HASH', '93f6b8ce4bb0ab61b4c7e42187f2aa64'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , '6785205860:AAF8ALVabwc4pvTF0JRNJoNkzDObx-QkciA'))
+    API_ID = int(getenv('API_ID', ''))
+    API_HASH = str(getenv('API_HASH', ''))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , ''))
     name = str(getenv('name', 'kanha_file2link_bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002049098708'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002049098708'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002268800628'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002268800628'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "1664376941").split()]
+    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'kanhaiyalalmeenakuwal'))
@@ -39,8 +39,8 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "https://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://meenakanhaiyalal638:files@files.8ko8k.mongodb.net/?retryWrites=true&w=majority&appName=files'))
+    DATABASE_URL = str(getenv('DATABASE_URL', ''))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Sonickuwalupdate')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
-    BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "-1002049098708")).split()))   
+    BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "-1002268800628")).split()))   
     BAN_ALERT = str(getenv('BAN_ALERT' , '<b>ʏᴏᴜʀ ᴀʀᴇ ʙᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ʙᴏᴛ.Pʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ @Sonickuwalupdate ᴛᴏ ʀᴇsᴏʟᴠᴇ ᴛʜᴇ ɪssᴜᴇ!!</b>'))
